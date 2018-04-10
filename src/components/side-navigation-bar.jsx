888d068d97b470d9b59a456e19f3f27d9ad86da7
Allan Chua <allanchua.officefiles@gmail.com>
2018-04-11 01:57:20 +0800

    -Fixed ES lint configurations

--- src/components/side-navigation-bar.jsx
+++ src/components/side-navigation-bar.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 class SideNav extends React.Component {
     render() {
         return (
-            <div class="side-nav-bar">
-            </div>
+            <div className="side-nav-bar" />
         );
     }
 }
