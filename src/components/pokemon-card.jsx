ad7d58e826a5d249b866a9c80f51baa283d9bf0f
Allan Chua <allanchua.officefiles@gmail.com>
2018-04-12 00:53:23 +0800

    -Finalized card styles

--- src/components/pokemon-card.jsx
+++ src/components/pokemon-card.jsx
@@ -4,8 +4,10 @@ class PokemonCard extends React.Component {
     render() {
         return (
             <div className='pokemon-card'>
-                <img src="/images/thm/001Bulbasaur.png" class="avatar" />
-                <p class='cardname'>001 - Bulbasaur</p>
+                <img src='/images/thm/002Ivysaur.png' class='avatar' />
+                <div class='info-panel'>
+                    <p class='cardname'>001 - Bulbasaur</p>
+                </div>
             </div>
         );
     }
