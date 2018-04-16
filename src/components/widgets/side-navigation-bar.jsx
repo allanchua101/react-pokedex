4a193990d040219bd1eadd230619332194bb0102
Allan Chua <allanchua.officefiles@gmail.com>
2018-04-16 08:04:17 +0200

    -Implemented navigation bar

--- src/components/widgets/side-navigation-bar.jsx
+++ src/components/widgets/side-navigation-bar.jsx
@@ -22,6 +22,9 @@ function buildSortDirections() {
 class SideNav extends React.Component {
     constructor(props) {
         super();
+        this.state = {
+            isOpened: false
+        };
         this.typeQuery = null;
         this.idQuery = null;
         this.searchQuery = null;
@@ -33,6 +36,7 @@ class SideNav extends React.Component {
         this.onPokeNameChanged = this.onPokeNameChanged.bind(this);
         this.onSortFieldChanged = this.onSortFieldChanged.bind(this);
         this.onSortDirectionChanged = this.onSortDirectionChanged.bind(this);
+        this.onHamburgerClicked = this.onHamburgerClicked.bind(this);
     }
     publishEvent() {
         this.filterTopic.publish({
@@ -63,6 +67,11 @@ class SideNav extends React.Component {
         this.sortDirection = JSON.parse(evt.target.value).Name;
         this.publishEvent();
     }
+    onHamburgerClicked() {
+        this.setState({
+            isOpened: !this.state.isOpened
+        });
+    }
     render() {
         let { poketypes } = this.props;
         let items = [];
@@ -71,23 +80,28 @@ class SideNav extends React.Component {
         items = items.concat(poketypes);
 
         return (
-            <div className='side-nav-bar opened'>
+            <div className={'side-nav-bar ' + (this.state.isOpened ? 'opened' : '')}>
+                <div className={'hamburger-menu ' + (this.state.isOpened ? 'opened' : '')} onClick={this.onHamburgerClicked}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                 <div className='side-nav-form'>
                     <h2>Configuration</h2>
                     <hr />
                     <h3>Filter List</h3>
                     <input type='text' className='filter-box' onChange={this.onPokeIdChanged} placeholder='ID' required='required' />
                     <input type='text' className='filter-box' onChange={this.onPokeNameChanged} placeholder='Pokemon Name' required='required' />
-                    <MaterializedSelect items={items} textProperty='ename' keyProperty='ename' 
-                                        label='Pokemon Type' onChange={this.onPokeTypeChanged} />
+                    <MaterializedSelect items={items} textProperty='ename' keyProperty='ename'
+                        label='Pokemon Type' onChange={this.onPokeTypeChanged} />
                     <hr />
                     <h3>Sort By</h3>
-                    <MaterializedSelect items={buildSortFields()} textProperty='Name' keyProperty='Name' 
-                                        label='Pokemon Type' onChange={this.onSortFieldChanged} />
+                    <MaterializedSelect items={buildSortFields()} textProperty='Name' keyProperty='Name'
+                        label='Pokemon Type' onChange={this.onSortFieldChanged} />
                     <hr />
                     <h3>Sort Direction</h3>
-                    <MaterializedSelect items={buildSortDirections()} textProperty='Name' keyProperty='Name' 
-                                        label='Sort Direction' onChange={this.onSortDirectionChanged} />
+                    <MaterializedSelect items={buildSortDirections()} textProperty='Name' keyProperty='Name'
+                        label='Sort Direction' onChange={this.onSortDirectionChanged} />
                 </div>
             </div>
         );
