9715e8acff6d86697da476612c7ee9aae620a9ad
Allan Chua <allanchua.officefiles@gmail.com>
2018-04-17 07:12:16 +0200

    -Implemented close button for pokemon cards

--- src/components/widgets/pokemon-card.jsx
+++ src/components/widgets/pokemon-card.jsx
@@ -15,29 +15,49 @@ class PokemonCard extends React.Component {
     constructor(props) {
         super();
         this.onPokemonCardTapped = this.onPokemonCardTapped.bind(this);
+        this.onPokemonCardClosed = this.onPokemonCardClosed.bind(this);
     }
     onPokemonCardTapped(evt) {
         let isExpanded = this.props.isExpanded;
-        
+
+        if (!isExpanded) {
+            this.props.messageBus.publish({
+                event: 'HIDE ALL',
+                originId: this.props.data.id
+            });
+        }
+    }
+    onPokemonCardClosed(evt) {
         this.props.messageBus.publish({
-            event: (isExpanded ? 'SHOW ALL' : 'HIDE ALL'),
+            event: 'SHOW ALL',
             originId: this.props.data.id
         });
     }
     render() {
         let { data, isVisible, isExpanded } = this.props;
-        let imageUrl = '/images/thm/' + data.id.toString() + fixBrokenImageNames(data.ename) + '.png';
+        let imageUrl = `/images/thm/${data.id.toString()}${fixBrokenImageNames(data.ename)}.png`;
+        let spriteUrl = `/images/spr/${data.id.toString()}MS.png`;
 
-        if(this.props.isExpanded)
-            imageUrl = '/images/img/' + data.id.toString() + fixBrokenImageNames(data.ename) + '.png';
+        if (this.props.isExpanded)
+            imageUrl = `/images/img/${data.id.toString()}${fixBrokenImageNames(data.ename)}.png`;
+
+        console.log(data);
 
         return (
-            <div className={'pokemon-card' + (isVisible ? '' : ' collapsed') + (isExpanded ? ' expanded' : '')} 
-                 title='Tap to see details' onClick={this.onPokemonCardTapped}>
+            <div className={'pokemon-card' + (isVisible ? '' : ' collapsed') + (isExpanded ? ' expanded' : '')}
+                title='Tap to see details' onClick={this.onPokemonCardTapped}>
                 <img src={imageUrl} className='avatar' />
+                <img src={spriteUrl} className='sprite' />
+                <a href="#" className="close-marker" onClick={this.onPokemonCardClosed} />
                 <div className='info-panel'>
                     <p className='card-number'>{data.id}</p>
                     <p className='cardname'>{data.ename}</p>
+                    <p className='card-prop show-on-expand'>Attack: {data.base.Attack}</p>
+                    <p className='card-prop show-on-expand'>Defense: {data.base.Defense}</p>
+                    <p className='card-prop show-on-expand'>HP: {data.base.HP}</p>
+                    <p className='card-prop show-on-expand'>Sp.Atk: {data.base['Sp.Atk']}</p>
+                    <p className='card-prop show-on-expand'>Sp.Def: {data.base['Sp.Def']}</p>
+                    <p className='card-prop show-on-expand'>Speed: {data.base.Speed}</p>
                 </div>
             </div>
         );
